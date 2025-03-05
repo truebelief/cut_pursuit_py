64a79ba5cca321124370b4ec66bb46c544570fe7
ZXi <truebelief2010@gmail.com>
2025-03-05 08:36:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ class BuildExt(build_ext):
 
 setup(
     name='cut_pursuit_py',
-    version='0.1.0',
+    version='0.1.1',
     author='Zhouxin Xi',
     author_email='truebelief2010@gmail.com',
     description='Cut Pursuit Algorithm for Point Cloud Segmentation',
